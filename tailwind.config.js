b151b1068f374af934d1ea87a723deff7d57cb84
Aika0828 <demattaangelicajoyce@gmail.com>
2025-11-03 10:50:17 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        background: '#F3F4F6',
+        text: '#111827',
+        primary: {
+          DEFAULT: '#1E3A8A',
+          foreground: '#F3F4F6',
+        },
+        secondary: {
+          DEFAULT: '#60A5FA',
+          foreground: '#111827',
+        },
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
